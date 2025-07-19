8da79de7797df7c455f0824cb18311df1497ef69
Alexandre <alexandrenas2003@gmail.com>
2025-07-19 14:59:00 -0300

    db seed posts, post-model

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
+"use client";
 
-export default function Home() {
+import clsx from "clsx";
+
+export default function HomePage() {
   return (
-    <div>
- 
+    <div
+      className={clsx(
+        "text-6xl",
+        "font-bold",
+        "text-blue-500",
+        "hover:text-blue-50",
+        "hover:bg-blue-500",
+        "transition",
+        "duration-300"
+      )}
+    >
+      <h1 onClick={() => alert(123)}>Oi</h1>
     </div>
   );
 }
